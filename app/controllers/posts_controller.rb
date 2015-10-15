04543f39bcf346b7ee27c9ff9418b5c208f9a88f
nilukasw <niluka_sennon@hotmail.com>
2015-10-16 09:42:00 +1100

    initialize repo

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+
+  def index
+    @content_first= ' This is some sample'
+    @content_last='This is more sample'
+  end
+
+  def edit
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+
+end
